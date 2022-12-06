b805bde2a86fb0d5c78c1fd492ca3f567eb02ad2
Tong2544 <94897662+Tong2544@users.noreply.github.com>
2022-12-06 21:27:48 +0700

    Add files via upload

---
+++ banner.php
@@ -0,0 +1,4 @@
+<div class="col-md-12" align="center" >
+        	<img src="banner/274231620_378309700432320_6123732301306273313_n.jpg" width="1110px" height="350px">
+            <!--size 980 x 200px --> 
+</div>
